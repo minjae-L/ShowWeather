fb2d3e41a33046491c401d4958cc798b2c28e744
이민재 <qlqlzbto@naver.com>
2024-05-28 21:03:52 +0900

    위도 경도를 격자x,y좌표로 변환하는 함수 구현

--- showWeather/showWeather/ViewController.swift
+++ showWeather/showWeather/ViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let TO_GRID = 0
+    let TO_GPS = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .cyan
+        print(APIManager.shared.convertGRID_GPS(mode: TO_GRID, lat_X: 37.4684021, lng_Y: 126.9340142))
     }
 
 
