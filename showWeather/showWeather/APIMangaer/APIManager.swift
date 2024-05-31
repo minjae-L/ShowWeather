0d912b08849f6558101e339ea0ef7a1331384c97
이민재 <qlqlzbto@naver.com>
2024-05-31 16:30:25 +0900

    JSON 데이터를 받고, 지정한 데이터모델로 변환

--- showWeather/showWeather/APIMangaer/APIManager.swift
+++ showWeather/showWeather/APIMangaer/APIManager.swift
@@ -32,72 +32,79 @@ class APIManager {
     init() {}
     
     // 변환된 x,y좌표의 url파싱
-    private func getUrl(nx: Int, ny: Int) -> URLComponents {
+    private func getUrl(nx: Int, ny: Int) -> [URLComponents] {
         let scheme = "https"
         let host = "apis.data.go.kr"
         let path = "/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst"
         
+        var arr = [URLComponents]()
         var components = URLComponents()
         components.scheme = scheme
         components.host = host
         components.path = path
-        components.percentEncodedQueryItems = [
-            URLQueryItem(name: "serviceKey", value: "BLSSs%2FqV7vhukX%2Bxy4ts3XEuFU6UVBP6EuwoUxoEkW%2FLMRW27dBTbJXTKUhWeWy9bNidunqwB9Gb8p0Gm3FTRw%3D%3D"),
-            URLQueryItem(name: "numOfRows", value: "50"),
-            URLQueryItem(name: "pageNo", value: "1"),
-            URLQueryItem(name: "dataType", value: "JSON"),
-            URLQueryItem(name: "base_date", value: "20240530"),
-            URLQueryItem(name: "base_time", value: "1700"),
-            URLQueryItem(name: "nx", value: "126"),
-            URLQueryItem(name: "ny", value: "33")
-        ]
-        return components
+        
+        for i in 1...2 {
+            components.percentEncodedQueryItems = [
+                URLQueryItem(name: "serviceKey", value: "BLSSs%2FqV7vhukX%2Bxy4ts3XEuFU6UVBP6EuwoUxoEkW%2FLMRW27dBTbJXTKUhWeWy9bNidunqwB9Gb8p0Gm3FTRw%3D%3D"),
+                URLQueryItem(name: "numOfRows", value: "30"),
+                URLQueryItem(name: "pageNo", value: String(i)),
+                URLQueryItem(name: "dataType", value: "JSON"),
+                URLQueryItem(name: "base_date", value: "20240530"),
+                URLQueryItem(name: "base_time", value: "1700"),
+                URLQueryItem(name: "nx", value: String(nx)),
+                URLQueryItem(name: "ny", value: String(ny))
+            ]
+            arr.append(components)
+        }
+        
+        return arr
     }
     
     private let session = URLSession(configuration: .default)
-    typealias NetworkResult = (Result<Data, NetworkError>) -> ()
+    typealias NetworkResult = (Result<WeatherDataModel, NetworkError>) -> ()
     
     // URLSession GET Data
     func dataFetch(nx: Int, ny: Int,completion: @escaping NetworkResult) {
         // URL 확인
-        guard let url = getUrl(nx: nx, ny: ny).url else {
-            completion(.failure(.invalidUrl))
-            return
+        let urls = getUrl(nx: nx, ny: ny)
+        for url in urls {
+            guard let url = url.url else {
+                completion(.failure(.invalidUrl))
+                return
+            }
+            var request: URLRequest = URLRequest(url: url)
+            request.httpMethod = "GET"
+            print("APIManager:: URL: \(url)")
+            let dataTask = session.dataTask(with: request) { data, response, error in
+                // 연결 확인
+                guard error == nil else {
+                    completion(.failure(.transportError))
+                    print(error?.localizedDescription)
+                    return
+                }
+                // 서버 확인
+                let successRange = 200..<300
+                guard let statusCode = (response as? HTTPURLResponse)?.statusCode else { return }
+                if !successRange.contains(statusCode) {
+                    completion(.failure(.serverError(code: statusCode)))
+                    return
+                }
+                // 데이터 확인
+                guard let loadData = data else {
+                    completion(.failure(.missingData))
+                    return
+                }
+                print("loadData: \(loadData)")
+                // 디코딩
+                do {
+                    let parsingData: WeatherDataModel = try
+                    JSONDecoder().decode(WeatherDataModel.self, from: loadData)
+                    completion(.success(parsingData))
+                } catch let error {
+                    completion(.failure(.decodingError(error: error)))
+                }
+            }.resume()
         }
-        print("APIManager:: URL: \(url)")
-        var request: URLRequest = URLRequest(url: url)
-        request.httpMethod = "GET"
-        
-        let dataTask = session.dataTask(with: request) { data, response, error in
-            // 연결 확인
-            guard error == nil else {
-                completion(.failure(.transportError))
-                print(error?.localizedDescription)
-                return
-            }
-            // 서버 확인
-            let successRange = 200..<300
-            guard let statusCode = (response as? HTTPURLResponse)?.statusCode else { return }
-            if !successRange.contains(statusCode) {
-                completion(.failure(.serverError(code: statusCode)))
-                return
-            }
-            // 데이터 확인
-            guard let loadData = data else {
-                completion(.failure(.missingData))
-                return
-            }
-            print("loadData: \(loadData)")
-            // 디코딩
-            do {
-                let parsingData: WeatherDataModel = try
-                JSONDecoder().decode(WeatherDataModel.self, from: loadData)
-                print(parsingData)
-                completion(.success(loadData))
-            } catch let error {
-                completion(.failure(.decodingError(error: error)))
-            }
-        }.resume()
     }
     // 위도 경도 변환함수
     func convertGRID_GPS(mode: Int, lat_X: Double, lng_Y: Double) -> LatXLngY {
--- showWeather/showWeather/ViewModels/WeatherViewModel.swift
+++ showWeather/showWeather/ViewModels/WeatherViewModel.swift
@@ -8,28 +8,77 @@
 import Foundation
 import MapKit
 
-enum DataInfo: String {
-    case temp = "T1H"
-    case rainAmount = "RN1"
-    case rainType = "PTY"
-    case skyInfo = "SKY"
-    case humidity = "REH"
-    case wind = "WSD"
-}
 struct WeatherModel {
-    
+    let date: String
+    var temp: String?
+    var rainAmount: String?
+    var rainType: String?
+    var skyInfo: String?
+    var humidity: String?
+    var wind: String?
 }
 class WeatherViewModel {
-    var elements: [ItemModel] = []
+    var elements: [WeatherModel] = [] {
+        didSet {
+            print("WVM:: elements didSet")
+        }
+    }
     var completion: MKLocalSearchCompletion? = nil {
         didSet {
             guard let completion = self.completion else { return }
             self.search(for: completion)
         }
     }
-    func convertDataFromCategory() {
-        
+    func convertDate(date: String, n: Int) -> String{
+        var num = Int(date)! + 100 * n
+        if num >= 2400 {
+            return String(num - 2400)
+        } else {
+            return String(num)
+        }
     }
+    func convertDataFromCategory(response: WeatherDataModel) {
+        var converted: [WeatherModel] = []
+        if self.elements.isEmpty {
+            guard var fcstTime = response.response.body?.items.item[0].fcstTime else { return }
+            for i in 0..<6 {
+                converted.append(WeatherModel(date: convertDate(date: fcstTime, n: i), temp: nil, rainAmount: nil, rainType: nil, skyInfo: nil, humidity: nil, wind: nil))
+            }
+        } else {
+            converted = self.elements
+        }
+        
+        if response.response.header.resultCode != "00" {
+            print("WeatherVM:: Data Error")
+            return
+        }
+        guard let arr = response.response.body?.items.item else { return }
+        var idx = 0
+        for i in arr {
+            var model = converted[idx]
+            switch i.category {
+            case "T1H":
+                model.temp = i.fcstValue
+            case "RN1":
+                model.rainAmount = i.fcstValue
+            case "SKY":
+                model.skyInfo = i.fcstValue
+            case "REH":
+                model.humidity = i.fcstValue
+            case "PTY":
+                model.rainType = i.fcstValue
+            case "WSD":
+                model.wind = i.fcstValue
+            default:
+                continue
+            }
+            converted[idx] = model
+            idx += 1
+            if idx > 5 { idx = 0 }
+        }
+        self.elements = converted
+    }
+    
     // MKLocalSearchRequest 생성 -> 선택된 지역의 정보 가져오기(위도,경도)
     func search(for suggestedCompletion: MKLocalSearchCompletion) {
         let searchRequest = MKLocalSearch.Request(completion: suggestedCompletion)
@@ -51,10 +100,11 @@ class WeatherViewModel {
             let location: LatXLngY = APIManager.shared.convertGRID_GPS(mode: 0, lat_X: lati, lng_Y: long)
             print("converted: \(location)")
             // URLSesison을 통한 네트워크 통신
-            APIManager.shared.dataFetch(nx: location.x, ny: location.y) { result in
+            APIManager.shared.dataFetch(nx: location.x, ny: location.y) {[weak self] result in
+                guard let self = self else { return }
                 switch result {
                 case .success(let data):
-                    print(data)
+                    self.convertDataFromCategory(response: data)
                 case .failure(.decodingError(error: let error)):
                     print("decodingError: \(error.localizedDescription)")
                 case .failure(.invalidUrl):
